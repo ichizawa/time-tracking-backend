6a77ecf4987f82b494371483578b37fdd7b0cd83
ichizawa <ryonuzuke@gmail.com>
2026-06-20 22:05:33 +0800

    test deploy backend

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ require("dotenv").config();
 
 const app = require("./src/app");
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+module.exports = app;
