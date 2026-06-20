14975fde26a3aeb05c264e06049681e074592e96
ichizawa <ryonuzuke@gmail.com>
2026-06-20 22:55:48 +0800

    changed process

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,10 +2,17 @@ const { initializeApp, cert } = require("firebase-admin/app");
 const { getFirestore, FieldValue } = require("firebase-admin/firestore");
 const { getAuth } = require("firebase-admin/auth");
 
-const serviceAccount = require("./timetracker-test-8746c-firebase-adminsdk-fbsvc-3febcd410b.json");
+// const serviceAccount = require("./timetracker-test-8746c-firebase-adminsdk-fbsvc-3febcd410b.json");
 
+// const app = initializeApp({
+//   credential: cert(serviceAccount),
+// });
 const app = initializeApp({
-  credential: cert(serviceAccount),
+  credential: cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  }),
 });
 
 const db = getFirestore(app);
