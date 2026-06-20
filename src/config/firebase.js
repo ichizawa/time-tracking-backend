b679a8e3c1cd4fdb8b2711449b00d20600e83fb1
ichizawa <ryonuzuke@gmail.com>
2026-06-20 21:50:09 +0800

    commited

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,12 +2,7 @@ const { initializeApp, cert } = require("firebase-admin/app");
 const { getFirestore, FieldValue } = require("firebase-admin/firestore");
 const { getAuth } = require("firebase-admin/auth");
 
-try {
-  const serviceAccount = require("../../timetracker-test-8746c-firebase-adminsdk-fbsvc-3febcd410b.json");
-  console.log("Firebase key loaded");
-} catch (err) {
-  console.error("Firebase key failed to load", err);
-}
+const serviceAccount = require("./timetracker-test-8746c-firebase-adminsdk-fbsvc-3febcd410b.json");
 
 const app = initializeApp({
   credential: cert(serviceAccount),
