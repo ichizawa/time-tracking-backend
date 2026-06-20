3fa2e657f4f83511fb122de6d559fb939da05590
ichizawa <ryonuzuke@gmail.com>
2026-06-20 21:44:01 +0800

    fixing issue vercel

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,7 +2,12 @@ const { initializeApp, cert } = require("firebase-admin/app");
 const { getFirestore, FieldValue } = require("firebase-admin/firestore");
 const { getAuth } = require("firebase-admin/auth");
 
-const serviceAccount = require("../../timetracker-test-8746c-firebase-adminsdk-fbsvc-3febcd410b.json");
+try {
+  const serviceAccount = require("../../timetracker-test-8746c-firebase-adminsdk-fbsvc-3febcd410b.json");
+  console.log("Firebase key loaded");
+} catch (err) {
+  console.error("Firebase key failed to load", err);
+}
 
 const app = initializeApp({
   credential: cert(serviceAccount),
